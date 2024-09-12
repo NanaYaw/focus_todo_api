720aeed491dcaaefa4e55a1081b571398e19f6b8
NanaYaw <ackam.noble@gmail.com>
2024-09-12 10:42:54 +0000

    Add: add user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   subject { create(:user) }
+
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_presence_of(:password) }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+
+  describe 'when email is not unique' do
+    it 'is not valid' do
+      duplicate_user = build(:user, email: subject.email.capitalize)
+      duplicate_user.valid?
+      expect(duplicate_user.errors[:email]).to include('has already been taken')
+    end
+  end
 end
